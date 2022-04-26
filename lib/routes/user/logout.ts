d093f9dbbd9394172328e9dc719bcaa093523992
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 10:39:47 +0300

    moved up and renamed

---
+++ lib/routes/user/logout.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express"
+import { deleteToken } from "../../db/queries/token/deleteToken"
+
+export default async function logout(req: Request, res: Response){
+    const refreshToken = req.cookies.refreshToken
+
+    try{
+        await deleteToken(refreshToken)
+
+        res.status(200)
+        res.clearCookie('refreshToken')
+        return res.json(process.env.loggedOutMessage)
+    } catch(e){
+        console.error(e)
+        return res.sendStatus(500)
+    }
+}
