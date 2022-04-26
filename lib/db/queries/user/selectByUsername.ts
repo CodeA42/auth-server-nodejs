af05d3af8c84bfb14d016d9d414fd92543c342aa
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 10:31:53 +0300

    moved up and renamed

--- lib/db/queries/user/selectByUsername.ts
+++ lib/db/queries/user/selectByUsername.ts
@@ -1,7 +1,7 @@
-import { AppDataSource } from "../../.."
-import MissingUsernameError from "../../../../error/MissingUsernameError"
-import UserNotFoundError from "../../../../error/UserNotFoundError"
-import User from "../../../Entities/user/User.Entity"
+import { AppDataSource } from "../.."
+import MissingUsernameError from "../../../error/MissingUsernameError"
+import UserNotFoundError from "../../../error/UserNotFoundError"
+import User from "../../Entities/user/User.Entity"
 
 export default async function selectByUsername(username: string): Promise<User | null> {
     if(!username) throw new MissingUsernameError(MissingUsernameError.defaultMessage)
