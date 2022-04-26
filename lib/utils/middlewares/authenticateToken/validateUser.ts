4970d355c9a789c51cc7a6dbc3f7b35fda500a66
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 10:42:49 +0300

    next is of type NextFunction

--- lib/utils/middlewares/authenticateToken/validateUser.ts
+++ lib/utils/middlewares/authenticateToken/validateUser.ts
@@ -1,21 +1,21 @@
-import { verify } from "jsonwebtoken";
-import { Request, Response } from "express";
-import { DecodedToken } from "../../../common/types";
-import { deleteToken } from "../../../db/queries/token/delete/deleteToken";
+import { verify } from "jsonwebtoken"
+import { NextFunction, Request, Response } from "express"
+import { DecodedToken } from "../../../common/types"
+import { deleteToken } from "../../../db/queries/token/deleteToken"
 
-function validateUser(getToken: Function){
-    return (req: Request, res: Response, next) => {
-        const token = getToken(req);
+export default function validateUser(getToken: Function){
+    return (req: Request, res: Response, next: NextFunction) => {
+        const token = getToken(req)
         
         verify(token, getToken.prototype.tokenSecret, async (err: Error, decoded: DecodedToken) => {
             if(err) return res.sendStatus(403)
 
             if(Math.floor(Date.now() / 1000) > decoded.exp){
                 if(getToken.prototype.tokenSecret === process.env.REFRESH_TOKEN_SECRET){
-                    await deleteToken(token);
+                    await deleteToken(token)
                     res.clearCookie('refreshToken')
                 }
-                return res.sendStatus(403);
+                return res.sendStatus(403)
             }
             
             res.locals.user = decoded.user
@@ -23,5 +23,3 @@ function validateUser(getToken: Function){
         })
     }
 }
-
-export default validateUser;
