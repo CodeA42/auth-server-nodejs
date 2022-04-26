c968c314f65e58243ea94c0e18f508c03e4b30bc
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 10:33:14 +0300

    linters not installed for the 500th time

--- lib/routes/index.ts
+++ lib/routes/index.ts
@@ -1,15 +1,15 @@
-import { Router, Request, Response } from "express";
-import login from "./user/login";
-import register from "./user/register";
-import logout from "./user/logout";
-import refresh from "./user/refresh";
-import validateUser from "../utils/middlewares/authenticateToken/validateUser";
-import getRefreshToken from "../utils/middlewares/authenticateToken/getRefreshToken";
-import logoutEverywhere from "./user/logout/everywhere";
+import { Router, Request, Response } from "express"
+import login from "./user/login"
+import register from "./user/register"
+import logout from "./user/logout"
+import refresh from "./user/refresh"
+import validateUser from "../utils/middlewares/authenticateToken/validateUser"
+import getRefreshToken from "../utils/middlewares/authenticateToken/getRefreshToken"
+import logoutEverywhere from "./user/logoutEverywhere"
 
-const router: Router = Router();  
-router.post("/register", register);
-router.post("/login", login);
+const router: Router = Router()
+router.post("/register", register)
+router.post("/login", login)
 router.get("/logout", validateUser(getRefreshToken), logout)
 router.get("/refresh", validateUser(getRefreshToken), refresh)
 router.get("/logout-everywhere", validateUser(getRefreshToken), logoutEverywhere)
