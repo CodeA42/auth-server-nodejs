81d7bfb661d6b24e4947437862f6ad86238bcc86
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 10:43:09 +0300

    added a check to see if the token is sent

--- lib/utils/tokens/getExp.ts
+++ lib/utils/tokens/getExp.ts
@@ -1,6 +1,9 @@
-import { decode } from "jsonwebtoken";
+import { decode } from "jsonwebtoken"
+import MissingTokenError from "../../error/MissingTokenError"
 
 export default function getTokenExp(token: string): number {
-    const decoded: any = decode(token);
-    return decoded.exp;
+    if(!token) throw new MissingTokenError(MissingTokenError.defaultMessage)
+
+    const decoded: any = decode(token)
+    return decoded.exp
 }
