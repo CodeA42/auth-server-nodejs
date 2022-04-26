b5d5fff220b620d72e2017efe1965e086c22b6fc
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 10:20:05 +0300

    added some checks

--- lib/db/queries/user/select/byUsername.ts
+++ lib/db/queries/user/select/byUsername.ts
@@ -1,15 +1,19 @@
-import { AppDataSource } from "../../..";
-import User from "../../../Entities/user/User.Entity";
+import { AppDataSource } from "../../.."
+import MissingUsernameError from "../../../../error/MissingUsernameError"
+import UserNotFoundError from "../../../../error/UserNotFoundError"
+import User from "../../../Entities/user/User.Entity"
 
-export default async function selectByUsername(username: string): Promise<User[]> {
-    if(username !== undefined){
-        try{
-            return await AppDataSource.manager.find(User,{where:{
-                username
-            }});
-        } catch(e) {
-            console.log(e);
-        }
+export default async function selectByUsername(username: string): Promise<User | null> {
+    if(!username) throw new MissingUsernameError(MissingUsernameError.defaultMessage)
+    
+    try{
+        const user: User = await AppDataSource.manager.findOne(User,{where:{
+            username
+        }})
+        if(user) return user
+        throw new UserNotFoundError(UserNotFoundError.defaultMessage)
+    } catch(e) {
+        console.log(e)
+        return null
     }
-    return [];
 }
