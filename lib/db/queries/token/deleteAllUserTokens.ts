01b4686a9880d2b03d1664ef61695fd642d3ceac
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 14:56:11 +0300

    cant be bothered doing it now

--- lib/db/queries/token/deleteAllUserTokens.ts
+++ lib/db/queries/token/deleteAllUserTokens.ts
@@ -5,6 +5,7 @@ import Token from "../../Entities/token/Token.Entity"
 export async function deleteAllUserTokens(username: string) {
     if(!username) throw new MissingUsernameError(MissingUsernameError.defaultMessage)
     
+    //TODO check if items are deleted
     try{
         const result = await AppDataSource.manager.delete(Token,{username})
         return result
