72f04571f448aee360d7df118a32b8b1b05ea801
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 15:46:05 +0300

    abstracted connection info

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,14 +3,15 @@ import Token from "./Entities/token/Token.Entity"
 import User from "./Entities/user/User.Entity"
 import adminDoesNotExist from "./queries/user/admin"
 import { createAdmin } from "./queries/user/admin/create"
+import * as connection from "../config/db.connection.json"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "nodejsapi",
-    password: "supersecret",
-    database: "databaseforoperationsofentirenations",
+    host: connection.host,
+    port: connection.port,
+    username: connection.username,
+    password: connection.password,
+    database: connection.database,
     entities:[User, Token],
     synchronize: true
 });
@@ -24,8 +25,7 @@ AppDataSource.initialize().then(async () => {
         }
     } catch(e) {
         console.error(e)
-        console.error(e);
-        
+        console.error(e)
     }
 
     console.log(`Db init success`)
