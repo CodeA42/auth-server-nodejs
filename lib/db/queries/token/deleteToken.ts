8621e57994d879ce28d925e590981fad9f234536
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 10:30:16 +0300

    moved up its clearer to see

---
+++ lib/db/queries/token/deleteToken.ts
@@ -0,0 +1,15 @@
+import { AppDataSource } from "../../.."
+import MissingTokenError from "../../../../error/MissingTokenError"
+import Token from "../../../Entities/token/Token.Entity"
+
+export async function deleteToken(token: string) {
+    if(!token) throw new MissingTokenError(MissingTokenError.defaultMessage)
+    
+    //TODO: check if item is deleted
+    try{
+        return await AppDataSource.manager.delete(Token,{token});
+    } catch(e) {
+        console.error(e)
+        return null
+    }
+}
