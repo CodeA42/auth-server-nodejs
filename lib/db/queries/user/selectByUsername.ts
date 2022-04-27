fbf7c57dcbdc37956aa7ac7e0917c52a90b5fb22
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 16:26:56 +0300

    no need to send default message

--- lib/db/queries/user/selectByUsername.ts
+++ lib/db/queries/user/selectByUsername.ts
@@ -4,7 +4,7 @@ import UserNotFoundError from "../../../error/UserNotFoundError"
 import User from "../../Entities/user/User.Entity"
 
 export default async function selectByUsername(username: string): Promise<User | null> {
-    if(!username) throw new MissingUsernameError(MissingUsernameError.defaultMessage)
+    if(!username) throw new MissingUsernameError()
     
     try{
         const user: User = await AppDataSource.manager.findOne(User,{where:{
@@ -15,5 +15,5 @@ export default async function selectByUsername(username: string): Promise<User |
         console.log(e)
         return null
     }
-    throw new UserNotFoundError(UserNotFoundError.defaultMessage)
+    throw new UserNotFoundError()
 }
